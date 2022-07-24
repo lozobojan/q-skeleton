dbfc1f8f3b522742dd68c9e8c29c70bbb68afdbd
Bojan Lozo <bojanlo@maestralsolutions.com>
2022-07-24 14:21:55 +0200

    Installed GuzzleHTTP and created simple POST request to retrieve the token

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,13 +4,40 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response as Response;
 
 class AuthController extends Controller
 {
-    /*
+    /**
      * Attempt API login
+     *
+     * @throws GuzzleException
      */
     public function login(LoginRequest $request){
+        // TODO: implement error messages in blade file
+
+        $httpClient = new Client();
+        $response = $httpClient->post('https://symfony-skeleton.q-tests.com/api/v2/token', [
+                'json' => [
+                    'email' => 'ahsoka.tano@q.agency',
+                    'password' => 'Kryze4President',
+                ]
+            ]
+        );
+
+        if ($response->getStatusCode() == Response::HTTP_OK){
+            $responseParsed = json_decode($response->getBody());
+            Session::put('bearerToken', $responseParsed->token_key);
+
+            redirect()->route('');
+        }else{
+            Session::flush();
+            // TODO: throw custom exception
+            abort($response->getStatusCode());
+        }
 
     }
 }
